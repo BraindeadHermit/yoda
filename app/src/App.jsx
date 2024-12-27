589a395840f48406c21e093a20c41213cb66888a
Alfonso Maria Ferrara <alfonso.mferrara@gmail.com>
2024-12-27 12:54:31 +0100

    feat: Aggiornamento del contesto. Aggiuunto il campo field.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,39 +7,40 @@ import { MentorSearchForm } from './components/pages/mentor-search-form';
 import FileHomePage from './components/pages/FileHomePage';
 import FileAddDocument from './components/pages/fileAddDocument';
 import ModifyProfile from './components/pages/ModifyProfile';
-import HomePageMentee from './components/pages/HomePageMentee'
-import HomePageMentore from './components/pages/HomePageMentore'
+import HomePageMentee from './components/pages/HomePageMentee';
+import HomePageMentore from './components/pages/HomePageMentore';
 import InserireVideo from './components/pages/InserireVideo';
 import Video from './components/pages/Video';  // Componente per la lista dei video
 import DettaglioVideo from './components/pages/DettaglioVideo';
-
+import { AuthProvider } from './auth/auth-context'; 
 import MentorProfileForm from './components/pages/ModifyProfile';
 import DettagliUtenteWrapper from './components/pages/DettaglioUtente';
 import MatchingResultPage from './components/pages/MatchingResultPage';
-
 
 function App() {
   const db = app;
   console.log(db);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegistrationPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/profile" element={<MentorProfileForm/>}/>
-        <Route path="/mentorsearch" element={<MentorSearchForm />} />
-        <Route path="/contents" element={<FileHomePage />} />
-        <Route path="/addfile" element={<FileAddDocument />} />
-        <Route path="/HomePageMentee" element={<HomePageMentee />} />
-        <Route path="/HomePageMentore" element={<HomePageMentore />} />
-        <Route path="/dettagli/:userId" element={<DettagliUtenteWrapper/>} />
-        <Route path="/InserireVideo" element={<InserireVideo />} />
-        <Route path="/videos" element={<Video />} />
-        <Route path="/video/:id" element={<DettaglioVideo />} />
-        <Route path="/matchingpage" element={<MatchingResultPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<MentorProfileForm />} />
+          <Route path="/mentorsearch" element={<MentorSearchForm />} />
+          <Route path="/contents" element={<FileHomePage />} />
+          <Route path="/addfile" element={<FileAddDocument />} />
+          <Route path="/HomePageMentee" element={<HomePageMentee />} />
+          <Route path="/HomePageMentore" element={<HomePageMentore />} />
+          <Route path="/dettagli/:userId" element={<DettagliUtenteWrapper />} />
+          <Route path="/InserireVideo" element={<InserireVideo />} />
+          <Route path="/videos" element={<Video />} />
+          <Route path="/video/:id" element={<DettaglioVideo />} />
+          <Route path="/matchingpage" element={<MatchingResultPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
